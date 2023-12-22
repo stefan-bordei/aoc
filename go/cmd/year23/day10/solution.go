1e76f025e988bfe3b0a833ddc88ead658fe46477
stefan-bordei <stefan.bordei13@gmail.com>
2023-12-22 20:40:00 +0000

    stefanB[Go]: Add 2023/day10 Part2.

--- go/cmd/year23/day10/solution.go
+++ go/cmd/year23/day10/solution.go
@@ -8,26 +8,26 @@ import (
 )
 
 type Queue struct {
-    q []Point
+    q []*Point
 }
 
 func (q *Queue) len() int {
     return len(q.q)
 }
 
-func (p *Queue) Enqueue(x Point) {
+func (p *Queue) Enqueue(x *Point) {
     p.q = append(p.q, x)
 }
 
-func (p *Queue) Dequeue() Point {
+func (p *Queue) Dequeue() *Point {
     if len(p.q) == 0 {
-        return Point{}
+        return &Point{}
     }
     e := p.q[0]
     if len(p.q) > 1 {
         p.q = p.q[1:]
     } else {
-        p.q = []Point{}
+        p.q = []*Point{}
     }
     return e
 }
@@ -38,8 +38,9 @@ var east = "J-7S"
 var west = "L-FS"
 
 type Grid struct {
-    Contents [][]Point
-    Visited map[Point]bool
+    Contents [][]*Point
+    Ground []*Point
+    Visited map[*Point]bool
 }
 
 func (g *Grid) rows() int {
@@ -57,29 +58,41 @@ type Point struct {
     Y int
 }
 
-func (p *Point) Scan(g *Grid, q *Queue) {
+func (p *Point) Scan(g *Grid, q *Queue, s *[]rune) {
     if p.X != 0 && strings.ContainsRune(north, g.Contents[p.X-1][p.Y].Id) && strings.ContainsRune(south, p.Id) {
         if _, ok := g.Visited[g.Contents[p.X-1][p.Y]]; !ok {
             g.Visited[g.Contents[p.X-1][p.Y]] = true
             q.Enqueue(g.Contents[p.X-1][p.Y])
+            if p.Id == 'S' {
+               intersect(s, south)
+            }
         }
     }
     if p.X != g.rows() && strings.ContainsRune(south, g.Contents[p.X+1][p.Y].Id) && strings.ContainsRune(north, p.Id) {
         if _, ok := g.Visited[g.Contents[p.X+1][p.Y]]; !ok {
             g.Visited[g.Contents[p.X+1][p.Y]] = true
             q.Enqueue(g.Contents[p.X+1][p.Y])
+            if p.Id == 'S' {
+               intersect(s, north)
+            }
         }
     }
-    if p.Y != g.rows() && strings.ContainsRune(east, g.Contents[p.X][p.Y+1].Id) && strings.ContainsRune(west, p.Id) {
+    if p.Y != g.cols() && strings.ContainsRune(east, g.Contents[p.X][p.Y+1].Id) && strings.ContainsRune(west, p.Id) {
         if _, ok := g.Visited[g.Contents[p.X][p.Y+1]]; !ok {
             g.Visited[g.Contents[p.X][p.Y+1]] = true
             q.Enqueue(g.Contents[p.X][p.Y+1])
+            if p.Id == 'S' {
+               intersect(s, west)
+            }
         }
     }
     if p.Y != 0 && strings.ContainsRune(west, g.Contents[p.X][p.Y-1].Id) && strings.ContainsRune(east, p.Id) {
         if _, ok := g.Visited[g.Contents[p.X][p.Y-1]]; !ok {
             g.Visited[g.Contents[p.X][p.Y-1]] = true
             q.Enqueue(g.Contents[p.X][p.Y-1])
+            if p.Id == 'S' {
+               intersect(s, east)
+            }
         }
     }
 }
@@ -87,21 +100,40 @@ func (p *Point) Scan(g *Grid, q *Queue) {
 func Solve(d string) (string, string, error) {
     s := utils.SplitLines(strings.TrimSpace(d))
     sp := startingPoint(s)
-    g := Grid{Visited: map[Point]bool{}}
+    ss := []rune{'|', '-', '7', 'F', 'J', 'L'}
+    g := Grid{Visited: map[*Point]bool{}}
     for r, rc := range s {
-        m := []Point{}
+        m := []*Point{}
         for c, cc := range rc {
-            m = append(m, Point{Id: cc, X: r, Y: c})
+            m = append(m, &Point{Id: cc, X: r, Y: c})
         }
         g.Contents = append(g.Contents, m)
     }
 
-    partOne, err := partOne(&g, sp)
+    q := Queue{}
+    q.Enqueue(sp)
+    g.Visited[sp] = true
+
+    for q.len() > 0 {
+        tp := q.Dequeue()
+        tp.Scan(&g, &q, &ss)
+    }
+
+    // Update 'S' to actual value
+    for _, r := range g.Contents {
+        for _, c := range r {
+            if c.Id == 'S' {
+                c.Id = ss[0]
+            }
+        }
+    }
+
+    partOne, err := partOne(&g)
     if err != nil {
         return utils.NOT_DONE, utils.NOT_DONE, err
     }
 
-    partTwo, err := partTwo(&g, sp)
+    partTwo, err := partTwo(&g)
     if err != nil {
         return utils.NOT_DONE, utils.NOT_DONE, err
     }
@@ -109,33 +141,75 @@ func Solve(d string) (string, string, error) {
     return partOne, partTwo, nil
 }
 
-func partOne(g *Grid, sp Point) (string, error) {
-    res := 0
-    q := Queue{}
-    q.Enqueue(sp)
-    g.Visited[sp] = true
+func partOne(g *Grid) (string, error) {
+    res := len(g.Visited) / 2
 
-    for q.len() > 0 {
-        tp := q.Dequeue()
-        tp.Scan(g, &q)
+    return strconv.Itoa(res), nil
+}
+
+func partTwo(g *Grid) (string, error) {
+    res := 0
+    ng := ReplaceOutside(g)
+
+    for _, gr := range ng.Ground {
+        count := 0
+        for i := gr.Y; i >= 0; i-- {
+            if gr.Y == len(ng.Contents[0])-1 {
+                continue
+            }
+            if ng.Contents[gr.X][i].Id == '|' {
+                count++
+            }
+            if ng.Contents[gr.X][i].Id == 'J' {
+                count++
+            }
+            if ng.Contents[gr.X][i].Id == 'L' {
+                count++
+            }
+        }
+        if count % 2 != 0 && count > 0{
+            res ++
+        }
     }
-    res += len(g.Visited) / 2
-
     return strconv.Itoa(res), nil
 }
 
-func partTwo(g *Grid, sp Point) (string, error) {
-    res := 0
-    return strconv.Itoa(res), nil
-}
-
-func startingPoint(d []string) Point {
-    for r, rowch := range d {
-        for c, colch := range rowch {
-            if colch == 'S' {
-                return Point{Id:'S', X: r, Y: c}
+func intersect(r *[]rune, l string) {
+    ts := []rune{}
+    for _, rc := range *r {
+        for _, lc := range l {
+            if rc == lc {
+                ts = append(ts, rc)
             }
         }
     }
-    return Point{}
+    *r = ts
+}
+
+func ReplaceOutside(g *Grid) Grid {
+    rg := Grid{}
+    for r, row := range g.Contents {
+        m := []*Point{}
+        for c, ch := range row {
+            if _, ok := g.Visited[ch]; !ok {
+                rg.Ground = append(rg.Ground, ch)
+                m = append(m, &Point{Id: '.', X: r, Y: c})
+            } else {
+                m = append(m, ch)
+            }
+        }
+        rg.Contents = append(rg.Contents, m)
+    }
+    return rg
+}
+
+func startingPoint(d []string) *Point {
+    for r, rowch := range d {
+        for c, colch := range rowch {
+            if colch == 'S' {
+                return &Point{Id:'S', X: r, Y: c}
+            }
+        }
+    }
+    return &Point{}
 }
--- go/cmd/year23/day10/solution_test.go
+++ go/cmd/year23/day10/solution_test.go
@@ -20,6 +20,48 @@ SJLL7
 |F--J
 LJ.LJ`
 
+var example4 = `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........`
+
+var example5 = `..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........`
+
+var example6 = `.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...`
+
+var example7 =`FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L`
+
 func TestSolution(t *testing.T) {
     tests := []struct {
         name string
@@ -41,24 +83,60 @@ func TestSolution(t *testing.T) {
             input: example3,
             want: "8",
         },
+        {
+            name: "example4",
+            input: example4,
+            want: "4",
+        },
+        {
+            name: "example5",
+            input: example5,
+            want: "4",
+        },
+        {
+            name: "example6",
+            input: example6,
+            want: "8",
+        },
+        {
+            name: "example7",
+            input: example7,
+            want: "10",
+        },
     }
     for _, tt := range tests {
         t.Run(tt.name, func(t *testing.T) {
             if tt.name == "example1" {
                 if got, _, _ := Solve(tt.input); got != tt.want {
-                    t.Errorf("part1() = %v, want %v", got, tt.want)
+                    t.Errorf("part1a() = %v, want %v", got, tt.want)
                 }
             } else if tt.name == "example2" {
                 if got, _, _ := Solve(tt.input); got != tt.want {
-                    t.Errorf("part1a() = %v, want %v", got, tt.want)
+                    t.Errorf("part1b() = %v, want %v", got, tt.want)
                 }
             } else if tt.name == "example3" {
                 if got, _, _ := Solve(tt.input); got != tt.want {
-                    t.Errorf("part1b() = %v, want %v", got, tt.want)
+                    t.Errorf("part1c() = %v, want %v", got, tt.want)
+                }
+            } else if tt.name == "example4" {
+                if _, got, _ := Solve(tt.input); got != tt.want {
+                    t.Errorf("part2a() = %v, want %v", got, tt.want)
+                }
+            } else if tt.name == "example5" {
+                if _, got, _ := Solve(tt.input); got != tt.want {
+                    t.Errorf("part2b() = %v, want %v", got, tt.want)
+                }
+            } else if tt.name == "example6" {
+                if _, got, _ := Solve(tt.input); got != tt.want {
+                    t.Errorf("part2c() = %v, want %v", got, tt.want)
+                }
+            } else if tt.name == "example7" {
+                if _, got, _ := Solve(tt.input); got != tt.want {
+                    t.Errorf("part2d() = %v, want %v", got, tt.want)
                 }
             } else {
                 if _, got, _ := Solve(tt.input); got != tt.want {
-                    t.Errorf("part2() = %v, want %v", got, tt.want)
+                    t.Errorf("part2e() = %v, want %v", got, tt.want)
                 }
             }
 
